85cd7a411c2c6540dd20fadf81f3122634ba31d9
Yuri Anderson Iagi Vieira <mce_yvieira@uoldiveo.com>
2019-10-11 15:35:36 -0300

    Bean validations

--- src/main/java/br/com/mySpringBoot/api/model/Perfil.java
+++ src/main/java/br/com/mySpringBoot/api/model/Perfil.java
@@ -1,4 +1,38 @@
 package br.com.mySpringBoot.api.model;
 
-public class Perfil {
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String desc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String getAuthority() {
+        return desc;
+    }
 }
