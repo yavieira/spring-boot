098711264a79bbb987d33749698eeb140b609b08
Yuri Anderson Iagi Vieira <mce_yvieira@uoldiveo.com>
2019-10-11 15:36:07 -0300

    Novas dependências e Annotations da classe de inicialização

--- src/main/java/br/com/mySpringBoot/api/ApiApplication.java
+++ src/main/java/br/com/mySpringBoot/api/ApiApplication.java
@@ -2,8 +2,12 @@ package br.com.mySpringBoot.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableCaching
 public class ApiApplication {
 
 	public static void main(String[] args) {
