41bbc828d74d6a9f1adc4094a55c0c7c39b7eeec
Yuri Anderson Iagi Vieira <mce_yvieira@uoldiveo.com>
2019-10-11 15:34:08 -0300

    Habilitando o Spring Security

--- src/main/java/br/com/mySpringBoot/api/security/AuthService.java
+++ src/main/java/br/com/mySpringBoot/api/security/AuthService.java
@@ -1,4 +1,29 @@
 package br.com.mySpringBoot.api.security;
 
-public class AuthService {
+import br.com.mySpringBoot.api.model.Usuario;
+import br.com.mySpringBoot.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<Usuario> user = usuarioRepository.findByEmail(email);
+            if(user.isPresent()){
+                return user.get();
+            }
+        throw new UsernameNotFoundException("Usuário não encontrado");
+    }
 }
--- src/main/java/br/com/mySpringBoot/api/security/SecurityConfig.java
+++ src/main/java/br/com/mySpringBoot/api/security/SecurityConfig.java
@@ -1,4 +1,42 @@
 package br.com.mySpringBoot.api.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity //Habilita o Spring Security
+@Configuration //Na subida da aplicação, inicializa a classe de configuração
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthService authService;
+
+
+    //Autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    //Autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+        .and().formLogin();
+    }
+
+    //Recursos estáticos (js, css, jsp)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
+
 }
